09c4805e1dc9f9d76af4cd65807d313ea8aecad7
Andrew M. Bray (SeungYeon Choi) <a@vxz.me>
2020-02-26 23:24:43 +0900

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -4,6 +4,6 @@ set CLASSPATH=.;dist\*;dist\lib\*
 
 :gogo
 java -version
-java -Xms1500m -Xmx3000m -Dorg.whitestar.gateway_ip="10.0.0.1" -Dfile.encoding="UTF8" -Dwhitestar.servertype="old" -server server.Start
+java -Xms1500m -Xmx3000m -Dnashorn.args="--no-deprecation-warning" -Dfile.encoding="UTF8" -server server.Start
 ping 10.0.0.1 -n 2 > nul
 goto gogo
