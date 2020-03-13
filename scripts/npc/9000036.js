3d84c668435b706b6c18a130cd866ecf27d8c23c
Andrew M. Bray (SeungYeon Choi) <a@vxz.me>
2020-03-13 23:48:20 +0900

    Agent E recovered to original

--- scripts/npc/9000036.js
+++ scripts/npc/9000036.js
@@ -1,49 +1,43 @@
 ﻿/*
-needfix 단풍잎교환
+ NPCID : 9000036
+ ScriptName : A_office
+ NPCNameFunc : 요원 E
+ Location : 211000000 (엘나스산맥 - 엘나스)
+ Location : 251000000 (무릉도원 - 백초마을)
+ Location : 230000001 (아쿠아리움 - 중앙 홀)
+ Location : 105040300 (던전 - 슬리피우드)
+ Location : 221000000 (루더스호수 - 지구방위본부)
+ Location : 104000000 (빅토리아로드 - 리스항구)
+ Location : 120000000 (빅토리아로드 - 노틸러스 선착장)
+ Location : 250000000 (무릉도원 - 무릉)
+ Location : 261000000 (선셋로드 - 마가티아)
+ Location : 240000000 (미나르숲 - 리프레)
+ Location : 260000000 (버닝로드 - 아리안트)
+ Location : 102000000 (빅토리아로드 - 페리온)
+ Location : 222000000 (루더스호수 - 아랫마을)
+ Location : 101000000 (빅토리아로드 - 엘리니아)
+ Location : 103000000 (빅토리아로드 - 커닝시티)
+ Location : 100000000 (빅토리아로드 - 헤네시스)
 */
 
-var item = Array(2043701,2043801,2043201,2043301,2043001,2043101,2040804,2044101,2044001,2044301,2044401,2044201,2044601,2044501,2044801,2044901,2044701,4031456,2070005,2070006,2330003,2330004,2330005);
-var itemCost = Array(100,100,100,100,100,100,200,100,100,100,100,100,100,100,100,100,100,150,100,300,60,80,100);
-//4000630
-importPackage(net.sf.odinms.client);
-
-var status = 0;
-
-function start() {
-    status = -1;
-    action(1, 0, 0);
-}
-
+var status = -1;
 function action(mode, type, selection) {
-	if (mode == -1) {
-	cm.dispose();
+	if (mode == 1 && type != 1) {
+		status++;
 	} else {
-	if (mode == 0 && status == 0) {
-	cm.dispose();
-        return;
-        }
-        if (mode == 1)
-            status++;
-        else
-            status--;
-        if (status == 0) {
-	var sentence = "안녕하세요 저는 #r각종 소모품#k을 단풍잎을 받고 교환해주는 #b요원 E#k입니다. 원하시는것을 고르세요\r\n   #z4001126##k은 모든 몬스터를 잡을시 랜덤으로 드롭됩니다.  ";
-		for (var i = 0; i < item.length; i++) {
-		sentence += "\r\n#b#L" + i + "##v4001126#  " + itemCost[i] + "개#k   =  " + "#v" + item[i] + "#" + "#z" + item[i] + "#";
-		}
-	cm.sendSimple(sentence);
-	} else if (status == 1) {
-		if (cm.haveItem(4001126,itemCost[selection])) {
-		var tmp = "#v" + item[selection] +  "#" + "#b" + "  #t" + item[selection] + "##k을(를) 드렸습니다. 나중에 또 들러주세요~"; 
-		cm.gainItem(item[selection],1);
-		cm.gainItem(4001126,-itemCost[selection]);
-		cm.sendOk(tmp);
-		cm.dispose()
+		if (type == 1 && mode == 1) {
+			status++;
+			selection = 1;
+		} else if (type == 1 && mode == 0) {
+			status++;
+			selection = 0;
 		} else {
-		var stmp = "#v" + item[selection] +  "#" + "#b" + "  #t" + item[selection] + "##k을(를) 드리기에는 #r#b#i4001126##z4001126##k이 부족합니다!";
-		cm.sendOk(stmp);
-		cm.dispose();
+			cm.dispose();
+			return;
 		}
 	}
-    }
+	if (status == 0) {
+		cm.sendOk("안녕하신가요? 제게 무슨 볼일이시죠?");
+		cm.safeDispose();
+	}
 }
